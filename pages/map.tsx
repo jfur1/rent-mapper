a0bebbbccfc9125117ee00cab8f0b5eeda1c73c4
John Furlong <johnfurlong24@gmail.com>
2023-01-30 13:35:57 -0700

    remove hexGrid for now

--- pages/map.tsx
+++ pages/map.tsx
@@ -22,23 +22,23 @@ const Map : NextPage = () => {
       style: 'mapbox://styles/mapbox/dark-v11',
       center: [ -104.991531, 39.742043], // center map on Denver
       zoom: 12,
-      pitch: 55,
-      bearing: 25,
+      pitch: 45,
+      bearing: 20,
       attributionControl: false
     })
 
     // Add zoom buttons & compass
     const nav = new mapboxgl.NavigationControl();
     map.current.addControl(nav, 'top-right');
-    // Add the control to the map.
-    map.current.addControl(
-      new MapboxGeocoder({
-        accessToken: mapboxgl.accessToken,
-        mapboxgl: mapboxgl,
-        placeholder: "Search new city"
-      }),
-      'top-left'
-    );
+    // Add the geocodder control to the map.
+    // map.current.addControl(
+    //   new MapboxGeocoder({
+    //     accessToken: mapboxgl.accessToken,
+    //     mapboxgl: mapboxgl,
+    //     placeholder: "Search new city"
+    //   }),
+    //   'top-left'
+    // );
 
     // Initialize hexGrid
     var bbox = boundingBox(-104.991531, 39.742043, 6)
@@ -55,102 +55,102 @@ const Map : NextPage = () => {
 
     map.current.on('load', () => {
       // Create a hex grid
-      map.current.addLayer({
-        'id': 'hexGrid',
-        'type': 'fill',
-        'source': {
-            'type': 'geojson',
-            'data': hexGrid
-        },
-        'layout': {},
-        'paint': {
-            'fill-color': '#088',
-            'fill-opacity': [
-              "interpolate", ["linear"], ["get", "density"],
-              0, 0.3,
-              1, 1
-            ],
-        }
-      });
+      // map.current.addLayer({
+      //   'id': 'hexGrid',
+      //   'type': 'fill',
+      //   'source': {
+      //       'type': 'geojson',
+      //       'data': ReviewsData
+      //   },
+      //   'layout': {},
+      //   'paint': {
+      //       'fill-color': '#088',
+      //       'fill-opacity': [
+      //         "interpolate", ["linear"], ["get", "density"],
+      //         0, 0.3,
+      //         1, 1
+      //       ],
+      //   }
+      // });
 
-      // Plot geoJSON points from market_data
-      map.current.addLayer({
-        'id': 'market-data-layer',
-        'type': 'circle',
-        'source': {
-            'type': 'geojson',
-            'data': MarketData
-        },
-        'layout': {},
-        'paint': {
-          'circle-radius': 4,
-          'circle-stroke-width': 2,
-          'circle-color': 'red',
-          'circle-stroke-color': 'white'
-        }
-      });
+    //   // Plot geoJSON points from market_data
+    //   map.current.addLayer({
+    //     'id': 'market-data-layer',
+    //     'type': 'circle',
+    //     'source': {
+    //         'type': 'geojson',
+    //         'data': MarketData
+    //     },
+    //     'layout': {},
+    //     'paint': {
+    //       'circle-radius': 4,
+    //       'circle-stroke-width': 2,
+    //       'circle-color': 'red',
+    //       'circle-stroke-color': 'white'
+    //     }
+    //   });
         
-    // Give height to hex grid
-    map.current.addLayer({
-        'id': 'grid-extrusion',
-        'type': 'fill-extrusion',
-        'source': {
-          'type': 'geojson',
-          'data': hexGrid
-        },
-        'layout': {},
-        'paint' : {
-          // Get the `fill-extrusion-color` from the source `color` property.
-          'fill-extrusion-color': 'hsl(78, 51%, 73%)',
-          // Get `fill-extrusion-height` from the source `height` property.
-          'fill-extrusion-height': [
-            "interpolate", ["linear"], ["get", "height"],
-            // height is 500 (or less) -> extrusion-height will be 500px
-            500, 500,
-            // height is 501 (or greater) -> extrusion-height will be 1300px
-            501, 1300
-          ],
-          // Get `fill-extrusion-base` from the source `base_height` property.
-          'fill-extrusion-base': 1,
-          // Make extrusions slightly opaque to see through indoor walls.
-          'fill-extrusion-opacity': 0.5
-        }
-      })
-      // Create a popup, but don't add it to the map yet.
-      const popup = new mapboxgl.Popup({
-        "text-size": 24,
+    // // Give height to hex grid
+    // map.current.addLayer({
+    //     'id': 'grid-extrusion',
+    //     'type': 'fill-extrusion',
+    //     'source': {
+    //       'type': 'geojson',
+    //       'data': hexGrid
+    //     },
+    //     'layout': {},
+    //     'paint' : {
+    //       // Get the `fill-extrusion-color` from the source `color` property.
+    //       'fill-extrusion-color': 'hsl(78, 51%, 73%)',
+    //       // Get `fill-extrusion-height` from the source `height` property.
+    //       'fill-extrusion-height': [
+    //         "interpolate", ["linear"], ["get", "height"],
+    //         // height is 500 (or less) -> extrusion-height will be 500px
+    //         500, 500,
+    //         // height is 501 (or greater) -> extrusion-height will be 1300px
+    //         501, 1300
+    //       ],
+    //       // Get `fill-extrusion-base` from the source `base_height` property.
+    //       'fill-extrusion-base': 1,
+    //       // Make extrusions slightly opaque to see through indoor walls.
+    //       'fill-extrusion-opacity': 0.5
+    //     }
+    //   })
+    //   // Create a popup, but don't add it to the map yet.
+    //   const popup = new mapboxgl.Popup({
+    //     "text-size": 24,
       
-        closeButton: false,
-        closeOnClick: false
-      });
-      map.current.on('mouseenter', 'market-data-layer', (e) => {
-        // Change the cursor style as a UI indicator.
-        map.current.getCanvas().style.cursor = 'pointer';
+    //     closeButton: false,
+    //     closeOnClick: false
+    //   });
+    //   map.current.on('mouseenter', 'market-data-layer', (e) => {
+    //     // Change the cursor style as a UI indicator.
+    //     map.current.getCanvas().style.cursor = 'pointer';
         
-        // Copy coordinates array.
-        const coordinates = e.features[0].geometry.coordinates.slice();
-        const description = e.features[0].properties.CountyName;
+    //     // Copy coordinates array.
+    //     const coordinates = e.features[0].geometry.coordinates.slice();
+    //     const description = e.features[0].properties.CountyName;
         
-        // Ensure that if the map is zoomed out such that multiple
-        // copies of the feature are visible, the popup appears
-        // over the copy being pointed to.
-        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-        }
+    //     // Ensure that if the map is zoomed out such that multiple
+    //     // copies of the feature are visible, the popup appears
+    //     // over the copy being pointed to.
+    //     while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+    //       coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+    //     }
 
-        console.log(coordinates)
+    //     console.log(coordinates)
         
-        // Populate the popup and set its coordinates
-        // based on the feature found.
-        popup.setLngLat(coordinates)
-        .setHTML(`<h1>Hello World from ${description}!</h1>`)
-        .addTo(map.current);
-      });
+    //     // Populate the popup and set its coordinates
+    //     // based on the feature found.
+    //     popup.setLngLat(coordinates)
+    //     .setHTML(`<h1>Hello World from ${description}!</h1>`)
+    //     .addTo(map.current);
+    //   });
         
-        map.current.on('mouseleave', 'market-data-layer', () => {
-          map.current.getCanvas().style.cursor = '';
-          popup.remove();
-        });
+    //     map.current.on('mouseleave', 'market-data-layer', () => {
+    //       map.current.getCanvas().style.cursor = '';
+    //       popup.remove();
+    //     });
     });
 
   }, [])
