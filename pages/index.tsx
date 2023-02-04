cd8861eb43c2cf756e9ce08e535f373bf02940f2
John Furlong <johnfurlong24@gmail.com>
2023-02-03 20:20:35 -0700

    remove tsx extension from import

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.scss'
-import Map from './map.tsx'
+import Map from './map'
 
 const inter = Inter({ subsets: ['latin'] })
 
